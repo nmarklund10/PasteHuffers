4b5ba45f9c7d7c257d138de6ae5e20b32924fec5
EC2 Default User <ec2-user@ip-172-31-18-99.us-west-2.compute.internal>
2018-01-08 11:25:42 +0000

    Student Login

--- app/controllers/s_login_controller.rb
+++ app/controllers/s_login_controller.rb
@@ -10,22 +10,16 @@ class SLoginController < ApplicationController
         accessId = params["access"]
         puts params
         #Look for instructors with matching names in database
-        possibleAssignment = Assignment.find(accessId)
-        assignment = possibleAssignment[0]
-        if assignment == nil then
+        possibleAssignment = Assignment.where( id: accessId)
+        if possibleAssignment.length == 0 then
             #Return error if we do not find anybody
             render json: {"success" => false, "reason" => "Access code or Name is empty"}
             return
         end
         #If we have any list of instr, just grab the first one. Instructor Creation should check unqiuness
         
-        if assignment.accessId != accessId then
-            #If access code does not exist throw error
-            render json: {"success" => false, "reason" => "Access code or Name is empty"}
-            return
-        end            
         #Verified info, save instructor id into the session then redirect to dashboard
-        session["AUID"] = assignment.id
+        session["AUID"] = possibleAssignment[0].id
         session["SUID"] = username
         render json: {"success" => true}
     end
