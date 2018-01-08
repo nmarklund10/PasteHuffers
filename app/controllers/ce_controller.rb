cad39669edc80b577cfe9616a2025e8855e03fed
Neil Marklund <nmarklund10@hotmail.com>
2018-01-08 19:32:45 +0800

    Added error handling for getting skeleton code

--- app/controllers/ce_controller.rb
+++ app/controllers/ce_controller.rb
@@ -6,9 +6,30 @@ class CeController < ApplicationController
     
     #New object. Called when asking for input
     def getSkeletonCode
-        currentAssignment = Assignment.find(params["AUID"])
+        auid = params["AUID"]
+        if session["IUID"] == nil then 
+            render json: {"success" => false, "reason" => "No User Logged in"} 
+            return 
+        end
+        if auid == nil then 
+            render json: {"success" => false, "reason" => "Need an AUID"} 
+            return 
+        end
+        currentAssignment = Assignment.find(auid)
+        if (currentAssignment.length == 0)
+            render json: {"success" => false, "reason" => "Assignment with #{auid} does not exist!"}
+            return
+        end
         currentCourse = Course.find(currentAssignment.course_id)
+        if (currentCourse.length == 0)
+            render json: {"success" => false, "reason" => "Course with #{currentAssignment.course_id} does not exist!"}
+            return
+        end
         currentInstructor = Instructor.find(currentCourse.instructor_id)
+        if (currentInstructor.length == 0)
+            render json: {"success" => false, "reason" => "Course with #{currentInstructor.instructor_id} does not exist!"}
+            return
+        end
         code = get_skeleton_code(currentCourse.instructor_id, currentAssignment.course_id, params["AUID"])
         render json: {"success" => true, "code" => code}
     end
