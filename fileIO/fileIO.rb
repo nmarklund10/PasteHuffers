7f90f09790c9993a61ffd2390ed6559db1e32774
nmarklund10 <nmarklund10@hotmail.com>
2018-01-08 19:00:43 +0800

    Neil Assignment Finder (Backend) (#33)
    * Removed redundant function that saved program output as a file
    
    * prototype for sending skeleton code
    
    * Removed extra Tab Container include
    
    * Added missing curly brace
    
    * Removed duplicate header

--- fileIO/fileIO.rb
+++ fileIO/fileIO.rb
@@ -7,17 +7,17 @@ class FileIO
     def write_code(iuid,cuid,auid,suid,input)
         File.write("#{iuid}/#{cuid}/#{auid}/#{suid}/-code.txt", "#{input}")
     end
-    def write_skeleton_code(iuid,cuid,auid,suid,input)
-        File.write("#{iuid}/#{cuid}/#{auid}/#{suid}/-skeleton_code.txt", "#{input}")
+    def write_skeleton_code(iuid,cuid,auid,input)
+        File.write("#{iuid}/#{cuid}/#{auid}/-skeleton_code.txt", "#{input}")
     end
     
     def read_log(iuid,cuid,auid,suid)
-        File.read("#{iuid}/#{cuid}/#{auid}/#{suid}/-log.txt", "#{input}")
+        File.read("#{iuid}/#{cuid}/#{auid}/#{suid}/-log.txt")
     end
     def read_code(iuid,cuid,auid,suid)
-        File.read("#{iuid}/#{cuid}/#{auid}/#{suid}/-code.txt", "#{input}")
+        File.read("#{iuid}/#{cuid}/#{auid}/#{suid}/-code.txt")
     end
-    def get_skeleton_code(iuid,cuid,auid,suid)
-        File.read("#{iuid}/#{cuid}/#{auid}/#{suid}/-skeleton_code.txt", "#{input}")
+    def get_skeleton_code(iuid,cuid,auid)
+        File.read("#{iuid}/#{cuid}/#{auid}/-skeleton_code.txt")
     end
 end
