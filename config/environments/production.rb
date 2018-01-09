46f55383888330554cdd72402672ea5ee7764081
matorin57 <matorin57@gmail.com>
2018-01-09 11:51:43 +0800

    Fixes new assignment mulitple dialog bug (#39)
    * Fixes new assignment mulitple dialog bug
    
    * Fix Heroku deployment
    
    * New Compiler
    
    * as
    
    * added procfile
    
    * procfile
    
    * cc
    
    * as
    
    * asd
    
    * no message
    
    * small
    
    * Deleting Local Dojo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,11 +25,12 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  #config.assets.compress = true
+  #config.assets.js_compressor = Closure::Compiler.new(compilation_level: 'ADVANCED_OPTIMIZATIONS')#Uglifier.new(harmony: true)
+  #config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Do not-not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
