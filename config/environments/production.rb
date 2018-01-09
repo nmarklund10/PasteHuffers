fb3511057d7a1846a74c0ec2e8caefe3f135c559
DESKTOP-9RHEF7F\matorin57 <matorin57@gmail.com>
2018-01-09 11:33:10 +0800

    asd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,9 +25,9 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.compress = true
-  config.assets.js_compressor = Closure::Compiler.new(compilation_level: 'ADVANCED_OPTIMIZATIONS')#Uglifier.new(harmony: true)
-  config.assets.css_compressor = :sass
+  #config.assets.compress = true
+  #config.assets.js_compressor = Closure::Compiler.new(compilation_level: 'ADVANCED_OPTIMIZATIONS')#Uglifier.new(harmony: true)
+  #config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
