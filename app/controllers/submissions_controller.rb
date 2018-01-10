b47c86c1ff47e4d5abc05a64c99536fbc8583694
matorin57 <matorin57@gmail.com>
2018-01-11 01:28:40 +0800

    Added assignment dashboard (#47)

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,9 +1,10 @@
 class SubmissionsController < ApplicationController
    
     def getSubmissions
-        auid = session["auid"]
+        auid = params["id"]
         if auid == nil then 
             render json: {"success" => false, "reason" => "No assignment id set, navigated to this page incorrectly."} 
+            return
         end
         @submissions = Submission.where(assignment_id: auid)
         render json: {"success" => true, "submissions" => @submissions} 
