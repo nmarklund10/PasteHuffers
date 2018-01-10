3d684c0008e27a1cd92300013f62d7ad3645fa3a
kraig9@outlook.com <kwo227@tamu.edu>
2018-01-10 05:54:13 +0000

    trying to write rendering tests

--- test/controllers/assignments_controller_test.rb
+++ test/controllers/assignments_controller_test.rb
@@ -4,4 +4,8 @@ class AssignmentsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "createAssignmentForm should render correct template and layout" do
+    assert_template :index
+    assert_template layout: "layouts/application"
+  end
 end
