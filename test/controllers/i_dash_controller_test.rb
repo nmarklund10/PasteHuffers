4b485eac2b21d63a093833dfc3c20daecae65428
kraig9@outlook.com <kwo227@tamu.edu>
2018-01-11 02:32:08 +0000

    changes

--- test/controllers/i_dash_controller_test.rb
+++ test/controllers/i_dash_controller_test.rb
@@ -9,7 +9,6 @@ class IDashControllerTest < ActionController::TestCase
     assert_select 'header','PasteHuffer'
   end
   
-<<<<<<< HEAD
   # test "should have course tabs" do
   #   get :index
   #   assert_select ''
@@ -25,20 +24,4 @@ class IDashControllerTest < ActionController::TestCase
   #   get :index
     
   # end
-=======
-  # test "should display course tabs" do
-  #   get :index
-  #   assert_select ''
-  # end
-  
-  test "should be able to add a new course" do
-    get :index
-    
-  end
-  
-  test "should have create assignment button" do
-    get :index
-    
-  end
->>>>>>> 2c04bef6ec4ae8f6dca7fae8f6cb4e55e947f156
 end
