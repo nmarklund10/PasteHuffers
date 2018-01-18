32eec6db6312900308a8dc10300dd282ea45b066
Garrigan Stafford <gstafford@tamu.edu>
2018-01-18 11:42:38 -0600

    PRe PR

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,15 @@
+require_relative '../../fileIO/fileIO'
 class Course < ActiveRecord::Base
   belongs_to :instructor
   has_many :assignments, dependent: :destroy
 
   def destroy
     #Destroy all the related files
+    begin
+      FileIO.delete_course(@instructor_id,@id)
+    rescue
+      #Do not care if file DNE, if file DNE then it is already deleted
+    end
     super
   end
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,8 +1,19 @@
+require_relative '../../fileIO/fileIO'
 class Submission < ActiveRecord::Base
   belongs_to :assignment
   
   def destroy
     #Destroy all the related files
+    begin
+      auid = @assignment_id
+      assign = Assignment.find(auid)
+      language = assign.language
+      cuid = assign.course_id
+      iuid = Course.find(cuid).instructor_id
+      FileIO.delete_submission(iuid,cuid,auid,@student_id,language)
+    rescue
+      #Do not care if file DNE
+    end
     super
   end
 end
