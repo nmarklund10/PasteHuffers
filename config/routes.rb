c8667f811ebbed6a8359754d6f4800a4292a942b
nmarklund10 <nmarklund10@hotmail.com>
2018-01-26 20:38:34 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # Login Routes
   #
   #root 'a_dash#index'
-  root 'ce#index'
+  root 'i_login#index'
   post 'ILogin/' => 'i_login#verifyCreds'
   get 'logout/'  => 'i_login#destroy'
 
