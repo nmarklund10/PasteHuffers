cc07a9833bc94c55b6abd575bf3d4454088a2ae3
Garrigan Stafford <gstafford@tamu.edu>
2018-02-05 15:38:37 -0600

    Admin Ruby Code

--- app/controllers/i_dash_controller.rb
+++ app/controllers/i_dash_controller.rb
@@ -7,6 +7,12 @@ class IDashController < ApplicationController
             render json: {"success" => false, "reason" => "No User signed in"}
             return
         end
+        #Check to see if the current instructor is an admin
+        if Admin.where(email: Instructor.find(session["IUID"]).email)[0] != nil then
+            @isAdmin = true
+        else
+            @isAdmin = false
+        end
         render "dashboard"
     end
     #upload skeleton code
@@ -42,5 +48,48 @@ class IDashController < ApplicationController
         file = params[:uploadedFile].read()
         FileIO.write_skeleton_code(@iuid, @cuid, @auid, file, @assignment.language)
         render json: {"success" => true}
+    end
+
+    def getWhiteList
+        begin
+            instructor = Instructor.find(session["IUID"])
+            if Admin.where(email: instructor.email)[0] == nil then
+                raise "Instructor is not admin"
+            end
+            render json: AcceptedInstructors.all
+        rescue
+            render json: {"success" => false, "reason" => "The logged in instructor does not have privelege for this resource"}
+            return
+        end
+    end
+
+    def addToWhiteList
+        begin
+            instructor = Instructor.find(session["IUID"])
+            if Admin.where(email: instructor.email)[0] == nil then
+                raise "Instructor is not admin"
+            end
+            AcceptedInstructors.create(email:params["email"])
+            render json: {"success" => true}
+            return
+        rescue
+            render json: {"success" => false, "reason" => "Invalid request"}
+            return
+        end
+    end
+
+    def deleteFromWhiteList
+        begin
+            instructor = Instructor.find(session["IUID"])
+            if Admin.where(email: instructor.email)[0] == nil then
+                raise "Instructor is not admin"
+            end
+            AcceptedInstructors.where(email:params["email"]).destroy_all
+            render json: {"success" => true}
+            return
+        rescue
+            render json: {"success" => false, "reason" => "Invalid request"}
+            return
+        end
     end
 end
