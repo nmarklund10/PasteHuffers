ee0c08882badf71c10c6e427804b727352893b9e
kraig9@outlook.com <kwo227@tamu.edu>
2018-02-07 02:57:15 +0000

    added timeout to code testing so it does not hang on programs asking for user input and added some visual features to the code editor

--- code_checker/code_checker.rb
+++ code_checker/code_checker.rb
@@ -6,9 +6,23 @@ class CodeChecker
     OUTPUT = 1
     def self.runCommand(cmd)
         #Runs command and returns if it is successful and stdout and stderr output
-        result = []
-        result, status = Open3.capture2e(cmd)
-        success = (status == 0)
+        puts "Running command: " + cmd
+        result = ""
+        success = -1
+        Open3.popen2e(cmd) { |input, output, wait_thr|
+            begin
+                Timeout.timeout(10) do
+                until output.eof? do
+                    result = output.read
+                end
+            end
+            rescue Timeout::Error
+                # process took longer than 10 seconds
+                Process.kill("KILL", wait_thr.pid)
+                result = "Program took too long to exit."
+            end
+            success = (wait_thr.value == 0)
+        }
         return [success, result]
     end
     
@@ -35,7 +49,6 @@ class CodeChecker
             if (@compile_result[SUCCESS])
                 filename = filename.chomp(".java")
                 @compile_result = runCommand("java " + filename)
-                runCommand("rm " + filename + ".class")
             end
         elsif (language == "C++" || language == "C")
             if (language == "C++")
@@ -47,7 +60,6 @@ class CodeChecker
             end
             if (@compile_result[SUCCESS])
                 @compile_result = runCommand(outFileName)
-                runCommand("rm " + outFileName)
             end      
         else
             return false
