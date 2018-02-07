aae7745ba32292188bac2eb7b03dbcfc3d325467
kraig9@outlook.com <kwo227@tamu.edu>
2018-02-07 07:02:21 +0000

    yet more merge conflicts

--- code_checker/code_checker.rb
+++ code_checker/code_checker.rb
@@ -29,10 +29,6 @@ class CodeChecker
     def self.testCode(language,suid,code)
         ext = FileIO.get_file_extension(language)
         tempFileWithCode = Tempfile.new([suid,ext], :encoding => 'ASCII-8BIT')
-<<<<<<< HEAD
-
-=======
->>>>>>> 6b0f2350e357ca44024359d73294e06db7ab1596
         code = code.encode('ASCII', invalid: :replace, undef: :replace, replace: "")
         tempFileWithCode.write(code)
         tempFileWithCode.flush()
